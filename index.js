36cf5bfa180ec6efb31b4d07f01ec8191189a57f
block-hac <cn.hashnet@gmail.com>
2020-12-18 17:52:34 +0800

    u1

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const rpc_methods = require('./src/methods');
 const app = express();
+const conf = require('./src/conf');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/', rpc_methods);
 
-const port=process.env.PORT;
+const port=conf.port;
 server = app.listen(port, () => console.log(`Server running on port ${port}`));
--- src/methods.js
+++ src/methods.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const Client = require('bitcoin-core');
 const conf = require('./conf');
 const bitcoin_conf = {
-    network: conf.net,
+    network: conf.rpc_net,
     username: conf.rpc_user,
     password: conf.rpc_pwd
 }
-const client = new Client();
-router.post('getblockcount', (req, res) => {
+console.log(bitcoin_conf)
+const client = new Client(bitcoin_conf);
+router.get('/test', (req, res) => {
+    console.log('backend works well!');
+});
+router.post('/getblockcount', (req, res) => {
     client.getBlockchainInformation().then((help) => {
         res.json(help);
     }).catch(err => {
