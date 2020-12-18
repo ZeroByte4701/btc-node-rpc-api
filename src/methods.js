703015b334203fe63a7af5cbe81d63feeae671b9
block-hac <cn.hashnet@gmail.com>
2020-12-18 18:09:31 +0800

    u1

--- src/methods.js
+++ src/methods.js
@@ -6,7 +6,8 @@ const bitcoin_conf = {
     network: conf.rpc_net,
     username: conf.rpc_user,
     password: conf.rpc_pwd,
-    port: conf.rpc_port
+    port: conf.rpc_port,
+    wallet: wallet.dat
 }
 console.log(bitcoin_conf)
 const client = new Client(bitcoin_conf);
