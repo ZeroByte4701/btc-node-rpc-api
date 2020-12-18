06d34fbaece073f8a7a757a78b3248fc4d90154c
block-hac <cn.hashnet@gmail.com>
2020-12-18 20:10:23 +0800

    u1

--- src/methods.js
+++ src/methods.js
@@ -27,12 +27,31 @@ router.post('/getblockchaininfo', (req, res) => {
     const url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
     const body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblockchaininfo', params: []});
     axios.post(url, body, headers)
-        .then(value => {
-            console.log(value);
+        .then(result => {
+            console.log(result.data);
+            res.json(result.data);
         })
         .catch(err => {
             console.error(err);
+            res.json(err);
         })
-})
+});
+
+router.post('/getblockhash', (req, res) => {
+    const headers = {
+        'content-type':'text/plain'
+    };
+    var url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
+    var body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblockhash', params: [req.body.blocknumber]});
+    axios.post(url, body, headers)
+        .then(result => {
+            console.log(result.data);
+            res.json(result.data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json(err);
+        })
+});
 
 module.exports = router;
