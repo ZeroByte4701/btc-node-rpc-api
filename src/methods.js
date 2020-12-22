489256ecb0e71661014e33db98a30d62beeaa990
block-hac <cn.hashnet@gmail.com>
2020-12-22 10:00:03 +0800

    u1

--- src/methods.js
+++ src/methods.js
@@ -5,10 +5,10 @@ const Client = require('bitcoin-core');
 const conf = require('./conf');
 const axios = require('axios');
 const bitcoin_conf = {
-    network: conf.rpc_net,
-    username: conf.rpc_user,
-    password: conf.rpc_pwd,
-    port: parseInt(conf.rpc_port),
+    network: 'mainnet',//conf.rpc_net,
+    username: 'coinpay',//conf.rpc_user,
+    password: 'coinpay2020',//conf.rpc_pwd,
+    port: 8332,//parseInt(conf.rpc_port),
     wallet: 'wallet.dat'
 }
 const headers = {
@@ -19,10 +19,30 @@ const client = new Client(bitcoin_conf);
 router.get('/test', (req, res) => {
     console.log('backend works well!');
 });
-router.post('/getblockcount', (req, res) => {
-    console.log(client.getBlockchainInformation());
+router.post('/getblockcount', (req, res) =>{
+    const url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
+    const body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblockcount', params: []});
+    axios.post(url, body, headers)
+        .then(result => {
+            res.json(result.data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json(err);
+        })
 });
-
+router.post('/getblock', (req, res) =>{
+    const url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
+    const body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblock', params: []});
+    axios.post(url, body, headers)
+        .then(result => {
+            res.json(result.data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json(err);
+        })
+});
 router.post('/getblockchaininfo', (req, res) => {
     const url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
     const body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblockchaininfo', params: []});
@@ -50,5 +70,20 @@ router.post('/getblockhash', (req, res) => {
             res.json(err);
         });
 });
+
+router.post('/getrawtransaction', (req, res) => {
+    var url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
+    var body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getrawtransaction', params: [req.body.blockhash]});
+    axios.post(url, body, headers)
+        .then(result => {
+            console.log(result.data);
+            res.json(result.data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json(err);
+        });
+})
+
 
 module.exports = router;
