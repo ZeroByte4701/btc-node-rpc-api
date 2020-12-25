f722be1549e140ea725da6bd051adeec2885e955
block-hac <cn.hashnet@gmail.com>
2020-12-26 00:18:39 +0800

    u1

--- src/methods.js
+++ src/methods.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-const Client = require('bitcoin-core');
 const conf = require('./conf');
 const axios = require('axios');
+var core_host = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
+var wallet_host = core_host + `/wallet/${conf.wallet_name}`;
 const headers = {
     'Content-Type': 'text/plain'
 };
 router.post('/getblockcount', (req, res) =>{
-    const url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
-    const body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblockcount', params: []});
-    axios.post(url, body, headers)
+    var body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblockcount', params: []});
+    axios.post(core_host, body, headers)
         .then(result => {
             res.json(result.data);
         })
@@ -20,9 +20,8 @@ router.post('/getblockcount', (req, res) =>{
         })
 });
 router.post('/getblock', (req, res) =>{
-    const url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
-    const body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblock', params: [req.body.blockhash]});
-    axios.post(url, body, headers)
+    var body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblock', params: [req.body.blockhash]});
+    axios.post(core_host, body, headers)
         .then(result => {
             res.json(result.data);
         })
@@ -32,9 +31,8 @@ router.post('/getblock', (req, res) =>{
         })
 });
 router.post('/getblockchaininfo', (req, res) => {
-    const url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
-    const body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblockchaininfo', params: []});
-    axios.post(url, body, headers)
+    var body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblockchaininfo', params: []});
+    axios.post(core_host, body, headers)
         .then(result => {
             console.log(result.data);
             res.json(result.data);
@@ -46,9 +44,8 @@ router.post('/getblockchaininfo', (req, res) => {
 });
 
 router.post('/getblockhash', (req, res) => {
-    var url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
     var body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getblockhash', params: [req.body.blocknumber]});
-    axios.post(url, body, headers)
+    axios.post(core_host, body, headers)
         .then(result => {
             console.log(result.data);
             res.json(result.data);
@@ -60,9 +57,8 @@ router.post('/getblockhash', (req, res) => {
 });
 
 router.post('/getrawtransaction', (req, res) => {
-    var url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}`;
     var body = JSON.stringify({jsonrpc:'1.0', id: 'curltext', method: 'getrawtransaction', params: [req.body.txhash, true]});
-    axios.post(url, body, headers)
+    axios.post(core_host, body, headers)
         .then(result => {
             console.log(result.data.vout);
             res.json(result.data.result.vout);
@@ -74,9 +70,8 @@ router.post('/getrawtransaction', (req, res) => {
 });
 
 router.post('/generate_new_address', (req, res) => {
-    var url = `http://${conf.rpc_user}:${conf.rpc_pwd}@${conf.rpc_url}:${conf.rpc_port}/wallet/${conf.wallet_name}`;
     var body = JSON.stringify({jsonrpc: "1.0", id: "curltext", method:"getnewaddress", params: ["","p2sh-segwit"]});
-    axios.post(url, body, headers)
+    axios.post(wallet_host, body, headers)
         .then(result => {
             body = JSON.stringify({jsonrpc: "1.0", id: "curltext", method:"dumpprivkey", params: [result.data.result]});  
             axios.post(url, body, headers)
@@ -98,7 +93,69 @@ router.post('/generate_new_address', (req, res) => {
         })
 });
 
-router.post
+router.post('/getbalances', (req, res) => {
+    var body = JSON.stringify({jsonrpc: "1.0", id: "curltext", method:"getbalances", params: []})
+    axios.post(wallet_host, body, headers)
+        .then(result => {
+            console.log(result.data);
+            res.json(result.data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json(err);
+        });
+});
 
+router.post('/getbalance', (req, res) => {
+    var body = JSON.stringify({jsonrpc: "1.0", id: "curltext", method: "getbalance", params: []});
+    axios.post(wallet_host, body, headers)
+        .then(result => {
+            console.log(result.data);
+            res.json(result.data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json(err);
+        });
+});
+
+router.post('/sendtoaddress', (req, res) => {
+    var body = JSON.stringify({jsonrpc: "1.0", id: "curltext", method: "sendtoaddress", params: [req.body.address, req.body.amount, " ", "withdrawal"]});
+    axios.post(wallet_host, body, headers)
+        .then(result => {
+            console.log(result.data);
+            res.json(result.data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json(err);
+        });
+});
+
+router.post('/getaddressinfo', (req, res) => {
+    var body = JSON.stringify({jsonrpc: "1.0", id: "curltext", method: "getaddressinfo", params: [req.body.address]});
+    axios.post(wallet_host, body, headers)
+        .then(result => {
+            console.log(result.data);
+            res.json(result.json);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json(err);
+        });
+});
+
+router.post('/getwalletinfo', (req, res) => {
+    var body = JSON.stringify({jsonrpc: "1.0", id: "curltext", method: "getwalletinfo", params: []});
+    axios.post(wallet_host, body, headers)
+        .then(result => {
+            console.log(result.data);
+            res.json(result.data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json(err);
+        });
+});
 
 module.exports = router;
