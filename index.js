9e68eeea598f2bce2eb1ed6b133567241e87ef44
block-hac <cn.hashnet@gmail.com>
2021-02-02 16:11:23 +0800

    secret

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser');
 const rpc_methods = require('./src/methods');
 const app = express();
 const conf = require('./src/conf');
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/', rpc_methods);
